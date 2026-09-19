16e6e4435bb77e5640af5f2a009c3d9600d3324b
Abhishek Thakur <abhishekthakur48700@gmail.com>
2026-09-19 15:09:03 +0530

    Time: 3 ms (95.85%), Space: 51.9 MB (88%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -5,15 +5,15 @@ class Solution {
         int max = Integer.MIN_VALUE;
 
         for(int j=0; j<nums.length; j++){
-            if(nums[j]==0){
+            if(nums[j]==0){ //pehle expand kro window 
                 zerocount++;
             }
 
-            while(zerocount>k){
+            while(zerocount>k){ // phir shrink kro jaba tak zerocount k ke equal na ho jaye
                 if(nums[i]==0)zerocount--;
                 i++;
             }
-            max = Math.max(max,j-i+1);
+            max = Math.max(max,j-i+1); // har point pe check kro length
             
         }
         return max;
